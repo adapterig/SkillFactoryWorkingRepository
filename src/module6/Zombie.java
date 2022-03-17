baa9168c541dcddac58d2ba95d53eca61fe21cf4
Ivan Antonov <ivan.antonov.alex@gmail.com>
2022-03-17 20:24:34 +0300

    Модуль 6 перегрузка и переопределение

--- src/module6/Zombie.java
+++ src/module6/Zombie.java
@@ -1,20 +1,27 @@
 package module6;
 
-public class Zombie extends Monster {
+import java.util.Locale;
 
+public class Zombie extends Monster {
+    public static String scream = "Raaaauuughhhh";
     public Zombie(String name) {
         super(name + " the Zombie", 5);
     }
+    // Overload this method to make SCREAMS
 
-    public void attack() {
-        super.attack();
-        this.growl();
-
-    }
-
-    public void growl() {
-        System.out.print("Raaaauuughhhh");
+    public void growl(boolean isLoud){
+        if (isLoud){
+            System.out.print(scream.toUpperCase());
+        }else {
+            System.out.print(scream);
+        }
         super.growl();
     }
-}
 
+    @Override
+    public void attack(){
+        super.attack();
+        growl();
+    }
+
+}
