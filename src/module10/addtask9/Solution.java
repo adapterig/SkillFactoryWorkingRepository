dc0886b2df800f1a8bdd2fbd1e6f324ada89a4b0
Ivan Antonov <ivan.antonov.alex@gmail.com>
2022-05-02 14:37:27 +0300

    Модуль 10 доп задача 9 начало

---
+++ src/module10/addtask9/Solution.java
@@ -0,0 +1,17 @@
+
+package module10.addtask9;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+    public static void solution(String district, int fromYear, int toYear) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new FileInputStream("src/module10/addtask9/crop_volumes.txt"));
+        ArrayList<String[]> Sttrings = new ArrayList<>();
+        while (scanner.hasNextLine()){
+            sc
+        }
+    }
+}
