286a6642185408bf677bc1fb6fc972a8d00561ea
Ivan Antonov <ivan.antonov.alex@gmail.com>
2022-05-02 08:34:28 +0300

    Модуль 10, доп Задача 6

---
+++ src/module10/addtask6/Main.java
@@ -0,0 +1,9 @@
+package module10.addtask6;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Solution.solution();
+    }
+}
---
+++ src/module10/addtask6/Solution.java
@@ -0,0 +1,55 @@
+package module10.addtask6;
+
+import javax.swing.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+    public static void solution() throws IOException {
+        Scanner scanner = new Scanner(new FileInputStream("src/module10/addtask6/data.txt"));
+        int diff = Integer.parseInt(scanner.nextLine());
+        StringBuilder text = new StringBuilder("");
+        ArrayList<String> strings = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            strings.add(scanner.nextLine());
+        }
+        int med = 0;
+
+        for (String s : strings) {
+            med += s.length();
+        }
+        med = med / strings.size();
+        text.append(med).append("\n");
+        for (String s : strings) {
+            if (Math.abs(s.length() - med) <= diff) {
+                text.append(s).append("\n");
+            }
+        }
+        System.out.println(text);
+        FileWriter writer = new FileWriter("src/module10/addtask6/output.txt");
+        writer.write(text.toString());
+        writer.close();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
